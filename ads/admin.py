1eb993545a1e695753222b101d849d7c359ebe88
belekabbasuulu <djonwick98@gmail.com>
2023-08-18 19:02:17 +0600

    created User, Category, Subcategory  models

--- ads/admin.py
+++ ads/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Ads
+from .models import Ads,Category,SubCategory
 
 
 admin.site.register(Ads)
+admin.site.register(Category)
+admin.site.register(SubCategory)
--- ads/models.py
+++ ads/models.py
@@ -1,11 +1,42 @@
 from django.db import models
+from django.utils import timezone
+
+from users.models import User
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class SubCategory(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Ads(models.Model):
+    LEARN = "Хочу научиться"
+    TEACH = "Могу научить"
+    TYPE_OF_ADS = [
+        (LEARN,"Хочу научиться"),
+        (TEACH,"Могу научить"),
+    ]
+
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True, blank=True, related_name='ads')
     title = models.CharField(max_length=150)
     description = models.TextField()
     price = models.FloatField()
-
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads', null=True, blank=True)
+    type = models.CharField(max_length=100,choices=TYPE_OF_ADS, default=LEARN)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
     def __str__(self) -> str:
         return self.title
 
