6f5f4ebd456004feee95b2fa4fe4be872e8e4679
belekabbasuulu <djonwick98@gmail.com>
2023-08-21 18:06:33 +0600

    added User model

--- ads/views.py
+++ ads/views.py
@@ -6,12 +6,6 @@ from .models import Ads
 
 def ads_list(request):
     all_ads = Ads.objects.all()
-    first = all_ads[0]
-    print(all_ads)
-    print(type(all_ads))
-    print(first.title)
-    print(first.description)
-    print(first.price)
     context = {
         "all_ads": all_ads
     }
