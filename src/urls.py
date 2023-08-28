98eec5454182330ea98bf92c9326e4222dd8050f
belekabbasuulu <djonwick98@gmail.com>
2023-08-28 18:48:13 +0600

    added user authentication

--- src/urls.py
+++ src/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ads/', include('ads.urls')),
+    path('user/', include('users.urls')),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,39 @@
+from typing import Any, Dict
+from django.urls import reverse_lazy
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegistrationForm, LoginForm
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = RegistrationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
+
+class LoginView(LoginView):
+    form_class = LoginForm
+    template_name = 'login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('ads-list')
+
+
+class LogoutView(LogoutView):
+    next_page = reverse_lazy('ads-list')
+    
+
+class ProfileView(TemplateView):
+    template_name = "profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
